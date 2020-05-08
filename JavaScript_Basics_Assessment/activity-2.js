7aa0f002a5fdd70010903ce61aa541e8a0273ffc
guzcruise <robertguzcruise@gmail.com>
2020-05-08 18:06:09 -0500

    progress

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,11 @@
+var name = prompt('What is your name');
+var n = name.length;
+
+if (n > 4) {
+
+    alert('Your name is greater than 4 characters');
+}
+else {
+
+    alert('Your name is less than 4 characters');
+}
